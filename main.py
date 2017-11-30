94a2d693cc659326993c4745a9bb12b2e3e10fa4
Katie <katcopp@yahoo.com>
2017-11-30 13:00:46 -0600

    finished assignment

--- main.py
+++ main.py
@@ -1,45 +1,19 @@
-from flask import Flask, request
+
+from flask import Flask, request, redirect
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<style>
-    .error {{color:red;}}
-</style>
-<!doctype html>
-<html>
-    <body>
-        <form action="/new-user" method="post">
-            <label for="username">Username:       </label>
-            <input id="username" type="text" name="username" value='{username}' /> 
-            <span class = "error"> {invalid_username}</span> <br> <br>
-
-            <label for="password">Password:       </label>
-            <input id="password1" type="password" name="password1" value='{password1}'/>
-            <span class = "error"> {invalid_password}</span> <br> <br>
-
-            <label for="password">Verify Password:</label>
-            <input id="password2" type="password" name="password2" value='{password2}'/>
-            <span class = "error"> {password_mismatch}</span> <br> <br>
-
-            <label for="email">Email (optional):</label>
-            <input id="email" type="text" name="email" value='{email}'/>
-            <span class = "error"> {invalid_email}</span> <br> <br>
-            <input type="submit" />
-        </form>
-
-
-    </body>
-</html>
-"""
-    
-
 @app.route("/")
 def index():
-    return form.format(username='', invalid_username='', password1='', 
-                invalid_password='', password2 ='', password_mismatch='',
-                email='', invalid_email='')
+    form = jinja_env.get_template('form.html')
+    return form.render()
 
 @app.route("/new-user", methods=['POST'])
 def new_user():
@@ -56,34 +30,31 @@ def new_user():
     if not username or len(username) < 3 or len(username)>20 or ' ' in username:
         invalid_username = "Please enter a valid username!"
         username = ''
-        password1 = ''
-        password2 = ''
         error_present= True
 
 
     if not password1 or len(password1) < 3 or len(password1)>20 or ' ' in password1:
         invalid_password = "Please enter a valid password!"
-        password1 = ''
         error_present= True
 
     if password1 != password2:
          password_mismatch = "Passwords don't match!!!"
-         password2 = ''
-         password1 = ''
          error_present= True
 
     if email:
         if '@' not in email or '.' not in email or ' ' in email or len(email) < 3 or len(email)>20:
             invalid_email = "Please enter a valid email!"
             email = ''
-            password1 = ''
-            password2 = ''
             error_present = True
     
     if not error_present:
-        return '<h1>Hello, ' + username + '! </h1>'
+        greeting = jinja_env.get_template('greeting.html')
+        return greeting.render(username= username)
     else:
-        return form.format(username=username, invalid_username=invalid_username, password1=password1, 
+        password1 =''
+        password2 =''
+        form = jinja_env.get_template('form.html')
+        return form.render(username=username, invalid_username=invalid_username, password1=password1, 
                 invalid_password=invalid_password, password2 =password2, password_mismatch= password_mismatch,
                 email=email, invalid_email=invalid_email)
 
